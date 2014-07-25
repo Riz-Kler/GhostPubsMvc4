cded223bcb13ac60d2664b7ef5396fcae496eec4
Phil Carney <phil.carney@cleardebt.co.uk>
2014-07-25 14:06:45 +0100

    update database did not work

---
+++ src/Carnotaurus.GhostPubsMvc.Data/Carnotaurus.GhostPubsMvc.Data/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Carnotaurus.GhostPubsMvc.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Carnotaurus.GhostPubsMvc.Data.CmsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Carnotaurus.GhostPubsMvc.Data.CmsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
