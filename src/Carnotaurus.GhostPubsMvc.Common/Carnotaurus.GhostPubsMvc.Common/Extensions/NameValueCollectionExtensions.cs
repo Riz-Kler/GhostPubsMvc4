0099d51fd661aec3a95356f23d1ec27977fc4247
Phil Carney <phil.carney@cleardebt.co.uk>
2014-12-05 19:46:10 +0000

    refactored collections

--- src/Carnotaurus.GhostPubsMvc.Common/Carnotaurus.GhostPubsMvc.Common/Extensions/NameValueCollectionExtensions.cs
+++ src/Carnotaurus.GhostPubsMvc.Common/Carnotaurus.GhostPubsMvc.Common/Extensions/NameValueCollectionExtensions.cs
@@ -7,20 +7,20 @@ namespace Carnotaurus.GhostPubsMvc.Common.Extensions
     public static class NameValueCollectionExtensions
     {
  
-        public static Int32 ToInt32(this NameValueCollection collection, String key)
+        public static Int32 ToInt32(this NameValueCollection nameValueCollection, String key)
         {
-            var result = collection[key].ToInt32();
+            var result = nameValueCollection[key].ToInt32();
 
             return result;
         }
 
-        public static IDictionary<string, string> ToDictionary(this NameValueCollection col)
+        public static IDictionary<string, string> ToDictionary(this NameValueCollection nameValueCollection)
         {
             IDictionary<string, string> results = new Dictionary<string, string>();
 
-            foreach (var key in col.AllKeys)
+            foreach (var key in nameValueCollection.AllKeys)
             {
-                results.Add(key, col[key]);
+                results.Add(key, nameValueCollection[key]);
             }
 
             return results;
